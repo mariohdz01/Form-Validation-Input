559543031804090c55e79fd18cde14777e12cae7
Mario Hernandez <mariohdz01@hotmail.com>
2021-02-28 18:15:25 -0600

    first commit

---
+++ src/main/java/com/tts/formvalidation/FormValidationApplication.java
@@ -0,0 +1,13 @@
+package com.tts.formvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormValidationApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(FormValidationApplication.class, args);
+	}
+
+}
